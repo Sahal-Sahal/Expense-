7190db4ed348a128fdf5c6110fd6e34187973f99
Sahal-Sahal <90123391+Sahal-Sahal@users.noreply.github.com>
2025-03-08 10:33:19 +0530

    Final commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from .import schemas,crud
 from .database import get_db
 import models
 
-app=FastAPI
+app=FastAPI()
 
 @app.post("/expenses/",response_model=schemas.ExpneseResponse)
 def create_expense(expense:schemas.ExpenseCreate, db:Session=Depends(get_db)):
@@ -14,10 +14,10 @@ def create_expense(expense:schemas.ExpenseCreate, db:Session=Depends(get_db)):
 def get_expenses(db:Session=Depends(get_db)):
     return crud.get_expenses(db=db)
 
-app.get("/expenses/month/{year}/{month}/",response_model=list[schemas.ExpneseResponse])
+@app.get("/expenses/month/{year}/{month}/",response_model=list[schemas.ExpneseResponse])
 def get_expenses_by_month(year:int,month:int,db:Session=Depends(get_db)):
     return crud.get_expenses_by_month(db=db,year=year,month=month)
 
-app.get("/totals/")
+@app.get("/totals/")
 def get_total_expenses(db:Session=Depends(get_db)):
     return crud.get_total_expenses(db=db)
